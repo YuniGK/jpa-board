0415824a10d0c2c5a8d8cda4e9bb448edad3a821
kim yunjung <1106qwe@hanmail.net>
2024-03-07 14:46:25 +0900

    @RepositoryRestResource

--- jpa-board/src/main/java/org/test/board/jpaboard/repository/ArticleRepository.java
+++ jpa-board/src/main/java/org/test/board/jpaboard/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package org.test.board.jpaboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.test.board.jpaboard.domain.Article;
 
+@RepositoryRestResource//컨트롤러를 만들지 않아도, 내부적으로 Rest API를 만들 수 있다.
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
