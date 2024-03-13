ba6756aaf9b615bf93ebb39eea1ad3cf692b5669
kim yunjung <1106qwe@hanmail.net>
2024-03-13 15:53:17 +0900

    설정 중
    https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html

--- jpa-board/src/main/java/org/test/board/jpaboard/config/SecurityConfig.java
+++ jpa-board/src/main/java/org/test/board/jpaboard/config/SecurityConfig.java
@@ -1,23 +1,48 @@
 package org.test.board.jpaboard.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+import org.test.board.jpaboard.domain.dto.UserAccountDto;
+import org.test.board.jpaboard.domain.dto.security.BoardPrincipal;
+import org.test.board.jpaboard.repository.UserAccountRepository;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                auth -> auth.anyRequest().permitAll()
-                )
-                .formLogin(
-                        (form) -> form.loginPage("/login").permitAll()
-                );
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(antMatcher("/"), antMatcher("/articles"), antMatcher("/articles/search-hashtag")).permitAll()
+                        .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
