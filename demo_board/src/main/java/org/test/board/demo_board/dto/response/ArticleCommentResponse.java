43da600abe9eb5ca2cc79a06156a5ee6941fb791
kim yunjung <1106qwe@hanmail.net>
2024-04-29 16:02:16 +0900

    dto_response
    @JsonProperty - Controller에서 json 데이터와 자바 엔티티 매핑을 위해 key를 매핑
    
    Comparable - 자기 자신과 매개변수 객체를 비교
    Comparator - 두 매개변수 객체를 비교
    Comparator.comparing - List의 원소들을 비교하여 정렬(내림차순)하는 것
    thenComparing - 2차 정렬
            https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html

--- demo_board/src/main/java/org/test/board/demo_board/dto/response/ArticleCommentResponse.java
+++ demo_board/src/main/java/org/test/board/demo_board/dto/response/ArticleCommentResponse.java
@@ -1,4 +1,59 @@
 package org.test.board.demo_board.dto.response;
 
-public record ArticleCommentResponse() {
+import org.test.board.demo_board.dto.ArticleCommentDto;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public record ArticleCommentResponse(
+        Long id,
+        String content,
+        LocalDateTime createdAt,
+        String email,
+        String nickname,
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
+) {
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        /*
+        * Comparable - 자기 자신과 매개변수 객체를 비교
+        * Comparator - 두 매개변수 객체를 비교
+        * Comparator.comparing - List의 원소들을 비교하여 정렬(내림차순)하는 것
+        * thenComparing - 2차 정렬
+        *
+        * https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html
+        * */
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
+    }
+
+    public static ArticleCommentResponse from(ArticleCommentDto dto) {
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+
+        return ArticleCommentResponse.of(
+                dto.id(),
+                dto.content(),
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname,
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
+        );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
+    }
 }
