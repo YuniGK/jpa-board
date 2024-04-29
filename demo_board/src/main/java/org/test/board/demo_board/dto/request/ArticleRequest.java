a7b4280211f51b051a7b99f08525365a53683107
kim yunjung <1106qwe@hanmail.net>
2024-04-29 16:07:23 +0900

    dto_request

--- demo_board/src/main/java/org/test/board/demo_board/dto/request/ArticleRequest.java
+++ demo_board/src/main/java/org/test/board/demo_board/dto/request/ArticleRequest.java
@@ -1,4 +1,30 @@
 package org.test.board.demo_board.dto.request;
 
-public record ArticleRequest() {
+import org.test.board.demo_board.dto.ArticleDto;
+import org.test.board.demo_board.dto.HashtagDto;
+import org.test.board.demo_board.dto.UserAccountDto;
+
+import java.util.Set;
+
+public record ArticleRequest(
+        String title,
+        String content
+) {
+    public static ArticleRequest of(String title, String content) {
+        return new ArticleRequest(title, content);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return toDto(userAccountDto, null);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtagDtos
+        );
+    }
+
 }
