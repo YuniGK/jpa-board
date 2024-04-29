3e2b9c0928a37678ceb84c20e4a840e832638b95
kim yunjung <1106qwe@hanmail.net>
2024-04-29 16:58:33 +0900

    -

--- demo_board/src/main/java/org/test/board/demo_board/controller/UserAccountManagementController.java
+++ demo_board/src/main/java/org/test/board/demo_board/controller/UserAccountManagementController.java
@@ -4,31 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.test.board.demo_board.dto.response.UserAccountResponse;
+import org.test.board.demo_board.service.UserAccountManagementService;
+import org.springframework.ui.Model;
 
 @RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
+
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
     public String userAccounts(Model model) {
-        model.addAttribute(
-                "userAccounts",
-                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
-        );
-
         return "management/user-accounts";
     }
 
     @ResponseBody
     @GetMapping("/{userId}")
     public UserAccountResponse userAccount(@PathVariable String userId) {
-        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+        return UserAccountResponse.from(null);
     }
 
     @PostMapping("/{userId}")
     public String deleteUserAccount(@PathVariable String userId) {
-        userAccountManagementService.deleteUserAccount(userId);
-
         return "redirect:/management/user-accounts";
     }
 }
