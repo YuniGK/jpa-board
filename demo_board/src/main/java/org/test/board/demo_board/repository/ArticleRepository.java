d7f631d2c56d609d20052f470e7c3e691c42cc2b
kim yunjung <1106qwe@hanmail.net>
2024-05-03 17:02:50 +0900

    @RepositoryRestResource

--- demo_board/src/main/java/org/test/board/demo_board/repository/ArticleRepository.java
+++ demo_board/src/main/java/org/test/board/demo_board/repository/ArticleRepository.java
@@ -1,8 +1,30 @@
 package org.test.board.demo_board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.test.board.demo_board.domain.Article;
 import org.test.board.demo_board.domain.ArticleComment;
+import org.test.board.demo_board.domain.QArticle;
+import org.test.board.demo_board.domain.projection.ArticleProjection;
+import org.test.board.demo_board.repository.querydsl.ArticleRepositoryCustom;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+/*
+* @RepositoryRestResource : repository 선언에 대한 어노테이션
+* ①collectionResourceRel : 반환되는 데이터 배열의 이름
+* ② path : 경로- jpa 와 같이 선언만으로도 기본적인 CRUD 가 만들어집니다. 여기서 차이는 controller가 필요 없다는 것 입니다.
+* - /{repository} 이런 식으로 맵핑이 되기 때문입니다.
+*
+* https://docs.spring.io/spring-data/rest/docs/3.0.5.RELEASE/reference/html/#getting-started.basic-settings
+*
+* @RepositoryRestResource(excerptProjection = Projection)
+* - @RepositoryRestResource의 excerptProjection으로 상시 설정할 Projection을 걸어 둘 수 있다.
+* */
+@RepositoryRestResource(excerptProjection = ArticleProjection.class)
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
 }
