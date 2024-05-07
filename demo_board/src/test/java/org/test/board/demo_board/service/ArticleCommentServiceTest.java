10cf1a62ec237f5976a7bea914b342f53d156956
kim yunjung <1106qwe@hanmail.net>
2024-05-07 16:56:12 +0900

    verify
    /* [ verify(T mock, VerificationMode mode) ]
            * times(n) - 몇 번이 호출됐는지 검증
            * never	- 한 번도 호출되지 않았는지 검증
            * atLeastOne - 최소 한 번은 호출됐는지 검증
            * atLeast(n) - 최소 n 번이 호출됐는지 검증
            * atMostOnce - 최대 한 번이 호출됐는지 검증
            * atMost(n) - 최대 n 번이 호출됐는지 검증
            * calls(n) - n번이 호출됐는지 검증 (InOrder랑 같이 사용해야 함)
            * only - 해당 검증 메소드만 실행됐는지 검증
            * timeout(long mills) - n ms 이상 걸리면 Fail 그리고 바로 검증 종료
            * after(long mills)	- n ms 이상 걸리는지 확인
            * timeout과 다르게 시간이 지나도 바로 검증 종료가 되지 않는다.
            * description - 실패한 경우 나올 문구 */

--- demo_board/src/test/java/org/test/board/demo_board/service/ArticleCommentServiceTest.java
+++ demo_board/src/test/java/org/test/board/demo_board/service/ArticleCommentServiceTest.java
@@ -1,6 +1,7 @@
 package org.test.board.demo_board.service;
 
 import groovy.util.logging.Slf4j;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -62,6 +63,91 @@ class ArticleCommentServiceTest {
                         tuple(2L, 1L, 1L, "child content")
                 );
         then(articleCommentRepository).should().findByArticle_Id(articleId);
+    }
+
+    @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
+    @Test
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
+        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
+
+        // When
+        sut.saveArticleComment(dto);
+
+        // Then
+        then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
+        /* [ verify(T mock, VerificationMode mode) ]
+        * times(n) - 몇 번이 호출됐는지 검증
+        * never	- 한 번도 호출되지 않았는지 검증
+        * atLeastOne - 최소 한 번은 호출됐는지 검증
+        * atLeast(n) - 최소 n 번이 호출됐는지 검증
+        * atMostOnce - 최대 한 번이 호출됐는지 검증
+        * atMost(n) - 최대 n 번이 호출됐는지 검증
+        * calls(n) - n번이 호출됐는지 검증 (InOrder랑 같이 사용해야 함)
+        * only - 해당 검증 메소드만 실행됐는지 검증
+        * timeout(long mills) - n ms 이상 걸리면 Fail 그리고 바로 검증 종료
+        * after(long mills)	- n ms 이상 걸리는지 확인
+        * timeout과 다르게 시간이 지나도 바로 검증 종료가 되지 않는다.
+        * description - 실패한 경우 나올 문구 */
+        then(articleCommentRepository).should(never()).getReferenceById(anyLong());//never() 한 번도 호출되지 않았는지 검증
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
+    }
+
+    @DisplayName("댓글 저장을 시도했는데 맞는 게시글이 없으면, 경고 로그를 찍고 아무것도 안 한다.")
+    @Test
+    void givenNonexistentArticle_whenSavingArticleComment_thenLogsSituationAndDoesNothing() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.articleId())).willThrow(EntityNotFoundException.class);
+
+        // When
+        sut.saveArticleComment(dto);
+
+        // Then
+        then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).shouldHaveNoInteractions();
+        then(articleCommentRepository).shouldHaveNoInteractions();
+    }
+
+    @DisplayName("부모 댓글 ID와 댓글 정보를 입력하면, 대댓글을 저장한다.")
+    @Test
+    void givenParentCommentIdAndArticleCommentInfo_whenSaving_thenSavesChildComment() {
+        // Given
+        Long parentCommentId = 1L;
+        ArticleComment parent = createArticleComment(parentCommentId, "댓글");
+        ArticleCommentDto child = createArticleCommentDto(parentCommentId, "대댓글");
+        given(articleRepository.getReferenceById(child.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(child.userAccountDto().userId())).willReturn(createUserAccount());
+        given(articleCommentRepository.getReferenceById(child.parentCommentId())).willReturn(parent);
+
+        // When
+        sut.saveArticleComment(child);
+
+        // Then
+        assertThat(child.parentCommentId()).isNotNull();
+        then(articleRepository).should().getReferenceById(child.articleId());
+        then(userAccountRepository).should().getReferenceById(child.userAccountDto().userId());
+        then(articleCommentRepository).should().getReferenceById(child.parentCommentId());
+        then(articleCommentRepository).should(never()).save(any(ArticleComment.class));
+    }
+
+    @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
+    @Test
+    void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment() {
+        // Given
+        Long articleCommentId = 1L;
+        String userId = "uno";
+        willDoNothing().given(articleCommentRepository).deleteByIdAndUserAccount_UserId(articleCommentId, userId);
+
+        // When
+        sut.deleteArticleComment(articleCommentId, userId);
+
+        // Then
+        then(articleCommentRepository).should().deleteByIdAndUserAccount_UserId(articleCommentId, userId);
     }
 
     /* init */
