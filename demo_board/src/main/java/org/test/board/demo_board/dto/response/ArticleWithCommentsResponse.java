cb8e055044d8f7a7e8d579eaab0e9e043f71ad81
kim yunjung <1106qwe@hanmail.net>
2024-05-08 16:28:48 +0900

    ArticeleWithComments

--- demo_board/src/main/java/org/test/board/demo_board/dto/response/ArticleWithCommentsResponse.java
+++ demo_board/src/main/java/org/test/board/demo_board/dto/response/ArticleWithCommentsResponse.java
@@ -1,51 +1,74 @@
 package org.test.board.demo_board.dto.response;
 
 import org.test.board.demo_board.dto.ArticleCommentDto;
+import org.test.board.demo_board.dto.ArticleWithCommentsDto;
+import org.test.board.demo_board.dto.HashtagDto;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
         Long id,
+        String title,
         String content,
+        Set<String> hashtags,
         LocalDateTime createdAt,
         String email,
         String nickname,
         String userId,
-        Long parentCommentId,
-        Set<ArticleCommentResponse> childComments
+        Set<ArticleCommentResponse> articleCommentsResponse
 ) {
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, Set<String> hashtags, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtags, createdAt, email, nickname, userId, articleCommentResponses);
     }
 
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
-        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
-                .comparing(ArticleCommentResponse::createdAt)
-                .thenComparingLong(ArticleCommentResponse::id);
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
-    }
-
-    public static ArticleCommentResponse from(ArticleCommentDto dto) {
+    public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
         String nickname = dto.userAccountDto().nickname();
         if (nickname == null || nickname.isBlank()) {
             nickname = dto.userAccountDto().userId();
         }
 
-        return ArticleCommentResponse.of(
+        return new ArticleWithCommentsResponse(
                 dto.id(),
+                dto.title(),
                 dto.content(),
+                dto.hashtagDtos().stream()
+                        .map(HashtagDto::hashtagName)
+                        .collect(Collectors.toUnmodifiableSet())
+                ,
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
                 dto.userAccountDto().userId(),
-                dto.parentCommentId()
+                organizeChildComments(dto.articleCommentDtos())
         );
     }
 
-    public boolean hasParentComment() {
-        return parentCommentId != null;
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+                    parentComment.childComments().add(comment);
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment())
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed()
+                                .thenComparingLong(ArticleCommentResponse::id)
+                        )
+                ));
     }
 }
